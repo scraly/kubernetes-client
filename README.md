f90e7ea75908cd822108bc6add52abf9eb04d401
scraly <scraly@gmail.com>
2021-07-08 13:53:05 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kubernetes-client
+
+Welcome in this Git repository.
+
+Work is in progress ut the aim of this repo is to find several samples for accessing and interacting with a Kubernetes cluster through several SDK, different languages.
---
+++ python/main.py
@@ -0,0 +1,23 @@
+import os
+from kubernetes import client
+
+# TOKEN
+## You can use a Service Account (SA) token
+token = "<my-token>"
+
+# KUBE
+configuration = client.Configuration()
+configuration.api_key["authorization"] = token
+configuration.api_key_prefix['authorization'] = 'Bearer'
+## you can execute kubectl cluster-info in order to know your Kubernetes cluster endpoint
+configuration.host = 'https://my-cluster.my-region.toto.io:443'
+configuration.ssl_ca_cert = 'k8s/aks/ca.crt'
+
+# Kubernetes client connection
+v1 = client.CoreV1Api(client.ApiClient(configuration))
+
+# Display the list of Pods for all namespaces
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
